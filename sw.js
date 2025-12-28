9f9047c06e9883117ccb12486f3d736c28cdac47
Carmelo Sammarco <sammarcocarmelo@gmail.com>
2025-12-28 13:24:19 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fishy-survival-v2';
+const CACHE_NAME = 'fishy-survival-v3';
 const ASSETS = [
   './',
   './index.html',
@@ -15,6 +15,20 @@ const ASSETS = [
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
 });
 
